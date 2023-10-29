eacb4ec6de397d131875bd8b5f321f7e14bf608f
JonnathanBC <jonnabcl56@gmail.com>
2023-10-29 12:03:32 -0500

    adventjs - challenge 18

--- retos/reto-18/index.js
+++ retos/reto-18/index.js
@@ -1,5 +1,9 @@
-export const dryNumber = (dry, numbers) => {
-  return Array.from(Array(numbers).keys())
-    .filter(number => `${number + 1}`.split('').includes(`${dry}`))
-    .map(n => n + 1)
+export const sortToys = (toys, positions) => {
+  return toys.sort((a, b) =>
+    positions[toys.indexOf(a)] - positions[toys.indexOf(b)]
+  )
 }
+
+// usamos un sort para poder ordenar el array de toys segun su indice.
+// aqui positions[toys.indexOf(a)], estamos encontrando el indice de cada toy segun el array de posiciones quearia 2, 3, 1, 0 ya "relacionado a su toy" es decir lo veremos tipo 'ball' position 2 etc
+// luego con esto positions[toys.indexOf(b)] obtenemos el mismo array 'ball' position 2 etc etc.   pero al colocarle - este ordenara de menor a mayor. (a sea menor que b)
